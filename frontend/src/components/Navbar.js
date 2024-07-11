3b84a2b5f5e18e7743b429fe794d8dbd28209564
poojak <139669968+pooja180801@users.noreply.github.com>
2024-07-11 13:34:25 +0530

    added components

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import ProfileInfo from './ProfileInfo'
+import { useNavigate } from 'react-router-dom';
+import SearchBar from './SearchBar';
+
+const Navbar = () => {
+
+  const [searchQuery,setSearchQuery]=useState("")
+  const navigate=useNavigate();
+
+  const onLogout=()=>{
+    navigate("/login");
+  };
+
+  const handleSearch=()=>{
+
+  }
+
+  const onClearSearch=()=>{
+    setSearchQuery("")
+  }
+
+
+  return (
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+        <h2 className='text-xl font-medium text-black'>Notes</h2>
+
+        <SearchBar value={searchQuery}
+        onChange={({target})=>{
+          setSearchQuery(target.value)
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+        />
+        <ProfileInfo onLogout={onLogout}/>
+      
+    </div>
+  )
+}
+
+export default Navbar
